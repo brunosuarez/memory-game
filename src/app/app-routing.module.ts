38fd3bad2a13d04d831079bb6bd9b119b5128c4b
Bruno Pereira Soares <bruno.soares@db1.com.br>
2020-07-09 00:23:51 -0300

    subindo os arquivos iniciais do projeto, criando página inicial do jogo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { GameComponent } from './components/game/game.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: GameComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
